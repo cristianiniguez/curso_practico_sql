6890206a75460a21522e991f7b7818e4299581fd
cristianiniguez <felizpc1@gmail.com>
2021-02-27 19:48:31 -0400

    ejercicios: el segundo mas alto

--- ejercicios/1. el primero.sql
+++ ejercicios/1. el primero.sql
@@ -1,12 +1,13 @@
--- Seleccionando el primero con FETCH
+-- Seleccionando el primer alumno
+-- con FETCH
 SELECT *
 FROM platzi.alumnos
 FETCH FIRST 1 ROWS ONLY;
--- Seleccionando el primero con LIMIT
+-- con LIMIT
 SELECT *
 FROM platzi.alumnos
 LIMIT 1;
--- Seleccionando el primero con Window Functions
+-- con Window Functions
 SELECT *
 FROM (
     SELECT ROW_NUMBER() OVER() AS row_id,
@@ -14,3 +15,20 @@ FROM (
     FROM platzi.alumnos
   ) AS alumnos_with_row_num
 WHERE row_id = 1;
+-- Seleccionando los primeros 5 alumnos
+-- con FETCH 
+SELECT *
+FROM platzi.alumnos
+FETCH FIRST 5 ROWS ONLY;
+-- con LIMIT
+SELECT *
+FROM platzi.alumnos
+LIMIT 5;
+-- con Windows Functions
+SELECT *
+FROM (
+    SELECT ROW_NUMBER() OVER() AS row_id,
+      *
+    FROM platzi.alumnos
+  ) AS alumnos_with_row_num
+WHERE row_id <= 5;
---
+++ ejercicios/2. el segundo mas alto.sql
@@ -0,0 +1,37 @@
+-- Seleccionando la segunda colegiatura más alta
+-- primera forma (con subquery en el WHERE)
+SELECT DISTINCT colegiatura
+FROM platzi.alumnos AS a1
+WHERE 2 = (
+    SELECT COUNT(DISTINCT colegiatura)
+    FROM platzi.alumnos AS a2
+    WHERE a1.colegiatura <= a2.colegiatura
+  );
+-- segunda forma (con LIMIT y OFFSET)
+SELECT DISTINCT colegiatura,
+  tutor_id
+FROM platzi.alumnos
+WHERE tutor_id = 20
+ORDER BY colegiatura DESC
+LIMIT 1 OFFSET 1;
+-- Seleccionando alumnos con la segunda colegiatura más alta
+--primera forma (con subquery e INNER JOIN)
+SELECT *
+FROM platzi.alumnos AS datos_alumnos
+  INNER JOIN (
+    SELECT DISTINCT colegiatura
+    FROM platzi.alumnos
+    WHERE tutor_id = 20
+    ORDER BY colegiatura DESC
+    LIMIT 1 OFFSET 1
+  ) AS segunda_mayor_colegiatura ON datos_alumnos.colegiatura = segunda_mayor_colegiatura.colegiatura;
+-- segunda forma (con subquery en el WHERE)
+SELECT *
+FROM platzi.alumnos AS datos_alumnos
+WHERE colegiatura = (
+    SELECT DISTINCT colegiatura
+    FROM platzi.alumnos
+    WHERE tutor_id = 20
+    ORDER BY colegiatura DESC
+    LIMIT 1 OFFSET 1
+  )
