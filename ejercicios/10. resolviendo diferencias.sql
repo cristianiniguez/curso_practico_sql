3ebe940a28720ee4f447b14746fec172c7bc6bba
cristianiniguez <felizpc1@gmail.com>
2021-02-28 19:18:20 -0400

    ejercicios: resolviendo diferencias

---
+++ ejercicios/10. resolviendo diferencias.sql
@@ -0,0 +1,20 @@
+-- Seleccionando número de alumnos por carrera
+-- mostrando id de carrera
+SELECT carrera_id,
+  COUNT(*) AS cuenta
+FROM platzi.alumnos
+GROUP BY carrera_id
+ORDER BY cuenta DESC;
+-- Eliminando carreras con id entre 30 y 40
+DELETE FROM platzi.carreras
+WHERE id BETWEEN 30 AND 40;
+-- Seleccionando alumnos cuya carrera no existe
+SELECT a.nombre,
+  a.apellido,
+  a.carrera_id,
+  c.id,
+  c.carrera
+FROM platzi.alumnos AS a
+  LEFT JOIN platzi.carreras AS c ON a.carrera_id = c.id
+WHERE c.id IS NULL
+ORDER BY a.carrera_id;
--- ejercicios/9. egoista - selfish.sql
+++ ejercicios/9. egoista - selfish.sql
@@ -18,3 +18,12 @@ FROM platzi.alumnos AS a
 GROUP BY tutor
 ORDER BY alumnos_por_tutor DESC
 LIMIT 10;
+-- Tarea: Seleccionando el promedio de alumnos por tutor
+SELECT AVG(alumnos_por_tutor) AS promedio_alumnos_por_tutor
+FROM (
+    SELECT CONCAT(t.nombre, ' ', t.apellido) AS tutor,
+      COUNT(*) AS alumnos_por_tutor
+    FROM platzi.alumnos AS a
+      INNER JOIN platzi.alumnos AS t ON a.tutor_id = t.id
+    GROUP BY tutor
+  ) AS alumnos_tutor;
